a1802caff1dd36eca5244c008e80c04686d9f1a4
Luis Souza <luishcs03@gmail.com>
2025-10-01 08:43:57 -0300

    organizing category_events

---
+++ events/category_events.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Dict, Any
+
+@dataclass(frozen=True)
+class CategoryCreated:
+    category_id: str
+    name: str
+    is_active: bool
+    timestamp: datetime = field(default_factory=datetime.now)
+
+@dataclass(frozen=True)
+class CategoryUpdated:
+    category_id: str
+    updated_fields: Dict[str, Any]
+    timestamp: datetime = field(default_factory=datetime.now)
+
+@dataclass(frozen=True)
+class CategoryActivated:
+    category_id: str
+    timestamp: datetime = field(default_factory=datetime.now)
+
+@dataclass(frozen=True)
+class CategoryDeactivated:
+    category_id: str
+    timestamp: datetime = field(default_factory=datetime.now)
